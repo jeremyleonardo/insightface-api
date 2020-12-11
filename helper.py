88a364db194e697a7ab81ef9a3548e111876d8f4
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-12-11 13:47:57 +0700

    add simple analyze image with url

---
+++ helper.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import urllib
+import urllib.request
+
+def url_to_image(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import insightface
+from helper import url_to_image
+import numpy as np
+
+app = FastAPI(title = "Insightface API")
+model = insightface.app.FaceAnalysis()
+model.prepare(ctx_id = -1, nms=0.4)
+
+@app.get("/")
+def root():
+    return {"message": "Insightface API web service is running"}
+
+@app.post("/analyze-image-url")
+async def analyze_image_url(url: str):
+    img = url_to_image(url)
+    faces = model.get(img)
+    flatenned_faces = []
+    for _, face in enumerate(faces):
+        gender = 'Male'
+        if face.gender==0:
+            gender = 'Female'
+        flatenned_faces.append(Face(age = face.age, gender = gender))
+        
+    return {
+        "message": "Success",
+        "faces": flatenned_faces
+    }
+
+class Face(object):
+    def __init__(self, age, gender):
+        self.age = age
+        self.gender = gender
