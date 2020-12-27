2952f9b072958826da50f2f987920db2cd6107bd
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-12-27 20:44:07 +0700

    add SKIP_DATABASE_CONNECTION_TEST

--- app/database.py
+++ app/database.py
@@ -27,17 +27,18 @@ def wait_database(engine):
     # Wait until database ready
     # Sometimes postgres is not ready yet because it needs to restart at init
 
-    success = 0
-    for i in range(30):
-        sleep(2)
-        try:
-            engine.execute('select 1')
-        except Exception as exc:
-            log.info("Waiting for database.")
-        else:
-            success += 1
-            log.info("Database connection tested.")
-        if(success == 3): 
-            log.info("Connection with database established.")
-            break
+    if(settings.SKIP_DATABASE_CONNECTION_TEST):
+        success = 0
+        for i in range(30):
+            sleep(2)
+            try:
+                engine.execute('select 1')
+            except Exception as exc:
+                log.info("Waiting for database.")
+            else:
+                success += 1
+                log.info("Database connection tested.")
+            if(success == 3): 
+                log.info("Connection with database established.")
+                break
         
