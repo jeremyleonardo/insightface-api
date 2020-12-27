b5b847f7804d31d6383968c6f56f421b9ce9aba6
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-12-27 17:48:33 +0700

    add SKIP_DATABASE_CONNECTION_TEST env

--- app/settings.py
+++ app/settings.py
@@ -7,3 +7,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 CREATE_ALL_EACH_RUN = True if os.getenv("CREATE_ALL_EACH_RUN", "True") == "True" else False
 DROP_ALL_EACH_RUN = True if os.getenv("DROP_ALL_EACH_RUN", "False") == "True" else False
 DEBUG = True if os.getenv("DEBUG", "False") == "True" else False
+SKIP_DATABASE_CONNECTION_TEST = True if os.getenv("SKIP_DATABASE_CONNECTION_TEST", "False") == "True" else False
