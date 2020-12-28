4b5e4b1f4a7212892a6bdbb9d6c2d7e694077d1c
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-12-28 11:17:15 +0700

    fix SKIP_DATABASE_CONNECTION_TEST validation

--- app/database.py
+++ app/database.py
@@ -27,7 +27,7 @@ def wait_database(engine):
     # Wait until database ready
     # Sometimes postgres is not ready yet because it needs to restart at init
 
-    if(settings.SKIP_DATABASE_CONNECTION_TEST):
+    if(settings.SKIP_DATABASE_CONNECTION_TEST is False):
         success = 0
         for i in range(30):
             sleep(2)
@@ -41,4 +41,3 @@ def wait_database(engine):
             if(success == 3): 
                 log.info("Connection with database established.")
                 break
-        
